6533d34d7cee96de6d6c14f2b23e5536b0c2b619
Precious Olusola <preciousolusola16@gmail.com>
2023-06-23 18:56:34 +0100

    DOne with task 1

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+#!/user/bin/pup
+# Create a file called 'school' in /tmp
+file { 'school':
+  ensure  => file,
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
+}
